9b9539f35ef279ac1bc8e789432b828eaf903c14
jacoblimzm <jacob_lim@live.com>
2021-03-06 18:39:01 +0800

    add update score and moves left functionality

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const $tileGrid = $(".tile-grid"); // assigning the query selector for the grid 
 const colors = ["red", "green", "blue", "yellow", "purple"]; // creating an array of colours so we can assign random colours to the tile divs in the HTML.
 let num = 8; // the size of the grid;
 let score = 0;
+let moveCount = 10;
 let blank = "rgb(255, 255, 255)";
 
 // ========== FUNCTION FOR CREATING THE BOARD ==========
@@ -158,6 +159,9 @@ const moveTilesDown = () => {
 
 // ========== COLOUR SWAPPING FUNCTIONALITY USING CLICK AND HOVER EVENT LISTENERS ==========
 
+let colorSwap1, colorSwap2, idSwap1, idSwap2; // creating variables to store the values of the background color and id for the selected tile, and the intended tile to swap with
+let validTilesSwap = [];
+
 const hoverSwap = (e) => {
   let tileElem = e.target; // grabbing the event object and the target, which is the div, because we are interested in the properties.
   // console.log(tileElem) // console.log to ensure we have grabbed the correct target. the DIV node should be logged.
@@ -168,15 +172,13 @@ const hoverSwap = (e) => {
 //   console.log(idSwap2) // console.log to make sure we have grabbed the right information
 };
 
-let colorSwap1, colorSwap2, idSwap1, idSwap2; // creating variables to store the values of the background color and id for the selected tile, and the intended tile to swap with
-let validTilesSwap = [];
 const clickSwap = (e) => {
   // need the event object which is passed into the callback function from the event listener. can access many properties of the element through the event object.
   let tileElem = e.target; // e.target is the way to access the properties with an anonymous function.
   let $tileElem = $(tileElem);
   let rightEdgeTiles = [7, 15, 23, 31, 39, 47, 55]; // don't want the RIGHT EDGE tiles to be able to be swapped with LEFT EDGE tile on following row
   let leftEdgeTiles = [0, 8, 16, 24, 32, 40, 48, 56]; // don't want the LEFT EDGE tiles to be able to be swapped with RIGHT EDGE tile on following row
-
+  
   if (colorSwap1 !== undefined && validTilesSwap.includes(idSwap2)) {
     // we only want to swap the colours if there is an existing colour stored in colorSwap1(which means a prior click has been made), AND the cursor is hovering over a different tile.
     // console.log($(".tile").eq(idSwap2)) // check we have selected the right jQuery object.
@@ -184,6 +186,8 @@ const clickSwap = (e) => {
     $(".tile").eq(idSwap1).css("background-color", colorSwap2); // using the id which is equivalent to the index, access the tile in the array and swap the colours.
     idSwap1 = null; // after the swap has been completed, reset the idSwap1.
     colorSwap1 = undefined; // reset colorSwap1 as well.
+    moveCount -= 1; // decrease count everytime move has been made
+    $("#moves").text(moveCount) // update the move counter.
   } else { // if there is no existing id stored in idSwap1 and color stored in colorSwap1, store upon click.
     // because this means game just started, or swap just been made.
     idSwap1 = parseInt($tileElem.attr("id"));
