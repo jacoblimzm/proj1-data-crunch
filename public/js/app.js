f8f48d4bfa2e9e77527a9a2f0abf30a5516bf3d5
jacoblimzm <jacob_lim@live.com>
2021-03-06 09:26:28 +0800

    move functions out of onready function and clean up

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,11 @@ const $tileGrid = $(".tile-grid");
 // assigning the query selector for the grid to a variable since it's gonna be used a lot.
 const colors = ["red", "green", "blue", "yellow", "purple"]; // creating an array of colours so we can assign random colours to the tile divs in the HTML.
 
-// ========== FUNCTION FOR CREATING THE BOARD ==========
 
+
+// ========== FUNCTION FOR CREATING THE BOARD ==========
 let num = 8;
+
 
 const randInt = () => {
   // random integer generator to be used for random colour picking. dependent on colors array.
@@ -20,19 +22,30 @@ const createTiles = () => {
     $divTile.addClass("tile").attr("id", i);
     //calling the random number function to pick random colours.
     $divTile.css("background-color", colors[randInt()]);
+
+    $divTile.on("click", clickSwap);
+    $divTile.on("mouseover", hoverSwap);
     // append each tile to the body.
     $tileGrid.append($divTile);
   }
 };
 
-$(() => {
-  createTiles(); // creating the tiles
+// ========== COLOUR SWAPPING FUNCTIONALITY USING CLICK AND HOVER EVENT LISTENERS ==========
 
-  // ========== COLOUR SWAPPING FUNCTIONALITY USING CLICK EVENT LISTENERS ==========
+const hoverSwap = (e) => {
+    let tileElem = e.target; // grabbing the event object and the target, which is the div, because we are interested in the properties.
+    // console.log(tileElem) // console.log to ensure we have grabbed the correct target. the DIV node should be logged.
+    let $tileElem = $(tileElem); // wrapping the vanilla JS node in money so it becomes a jQuery object and we can work with it.
+    colorSwap2 = $tileElem.css("background-color"); // store the colour so that we may use it to swap colours.
+    idSwap2 = Number($tileElem.attr("id")); // the attribute is returned as a string by default, so convert into a number.
+    // console.log(colorSwap2); // console.log to make sure we have grabbed the right information
+    // console.log(idSwap2) // console.log to make sure we have grabbed the right information
+  };
 
   let colorSwap1, colorSwap2, idSwap1, idSwap2; // creating variables to store the values of the background color and id for the selected tile, and the intended tile to swap with
   let validTilesSwap = [];
-  let clickSwap = (e) => {
+
+  const clickSwap = (e) => {
     // need the event object which is passed into the callback function from the event listener. can access many properties of the element through the event object.
     let tileElem = e.target; // e.target is the way to access the properties with an anonymous function.
     let $tileElem = $(tileElem);
@@ -50,15 +63,13 @@ $(() => {
     } else {
       // if there is no existing id stored in idSwap1 and color stored in colorSwap1, store upon click.
       // because this means game just started, or swap just been made.
-      idSwap1 = Number($tileElem.attr("id"));
+      idSwap1 = parseInt($tileElem.attr("id"));
       colorSwap1 = $tileElem.css("background-color");
-      // we also want to restrict the tiles that can be swapped to simply the tiles directly beside, above, and below it.
+      // ==== we also want to restrict the tiles that can be swapped to simply the tiles directly beside, above, and below it.
       if (rightEdgeTiles.includes(idSwap1)) { // if right edge tile is clicked, the valid tiles for swapping are only above, left, below
           validTilesSwap = [idSwap1-8, idSwap1-1, idSwap1+8]; 
-          
       } else if (leftEdgeTiles.includes(idSwap1)) { // if left edge tile is clicked, the valid tiles for swapping are only above, right, below
           validTilesSwap = [idSwap1-8, idSwap1+1, idSwap1+8];
-          
       } else {
           validTilesSwap = [idSwap1-8, idSwap1-1, idSwap1+1, idSwap1+8]; // all other tiles, above, left, right, below can be swapped.
           
@@ -69,22 +80,15 @@ $(() => {
     // console.log(idSwap1); // make sure we have grabbed the right info
   };
 
-  let hoverSwap = (e) => {
-    let tileElem = e.target; // grabbing the event object and the target, which is the div, because we are interested in the properties.
-    // console.log(tileElem) // console.log to ensure we have grabbed the correct target. the DIV node should be logged.
-    let $tileElem = $(tileElem); // wrapping the vanilla JS node in money so it becomes a jQuery object and we can work with it.
-    colorSwap2 = $tileElem.css("background-color"); // store the colour so that we may use it to swap colours.
-    idSwap2 = Number($tileElem.attr("id")); // the attribute is returned as a string by default, so convert into a number.
-    // console.log(colorSwap2); // console.log to make sure we have grabbed the right information
-    // console.log(idSwap2) // console.log to make sure we have grabbed the right information
-  };
 
-  let $tiles = $(".tile"); // returns ALL the jQuery nodes with .tile class. diff from a regular array. it is a collection.
-  $tiles.on("click", clickSwap); // apply the event listener to ALL tiles
-  $tiles.on("mouseover", hoverSwap); // apply the event listener to ALL tiles
-  //   tilesArr.forEach((tile) => {
-  //     // give each tile in the grid two event liseners, a click event and a mouseover event
-  //     tile.addEventListener("click", clickSwap); // the click event listener will either store information upon click, or swap colors if existing information is present.
-  //     tile.addEventListener("mouseover", hoverSwap); // the mouseover event will automatically store the information of whatever tile the user is hovering over for a swap.
-  //   });
+
+// ========== MAIN jQUERY FUNCTION ==========
+$(() => {
+  createTiles(); // creating the tiles
+  
+
+//   let $tiles = $(".tile"); // returns ALL the jQuery nodes with .tile class. diff from a regular array. it is a collection.
+//   $tiles.on("click", clickSwap); // apply the event listener to ALL tiles
+//   $tiles.on("mouseover", hoverSwap); // apply the event listener to ALL tiles
+  
 }); // end of jQuery onready function.
